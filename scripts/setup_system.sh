45d9df08ef2b9d44cd644e35b376d9c6746b648b
xxinran <xin-ran.wang@intel.com>
2021-10-11 14:50:01 +0800

    Update setup_system.sh
    remove skopeo pkg

--- scripts/setup_system.sh
+++ scripts/setup_system.sh
@@ -23,7 +23,7 @@
 # SOFTWARE.
 
 # Add skopeo sources
-. /etc/os-release
+#. /etc/os-release
 #echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list > /dev/null
 #curl --silent --show-error -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
 
